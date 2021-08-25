5cc99aa5d0a852bd2ee68fb41b5799e015a19f61
SGF-MMXXI <88549986+SGF-MMXXI@users.noreply.github.com>
2021-08-25 14:59:42 -0500

    Added a loop
    Added a loop that scans through the array for name and height and gives a special message to the tallest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
+//Heres an array of objects listing pokemon and some attributes
+
 let pokemonList = [{
-    name: 'Bulbasaur',
-    type:['grass', 'poison'],
-    height:2.4},
+        name: 'Bulbasaur',
+        type:['grass', 'poison'],
+        height:2.4},
     
-    {name: 'Charmander',
-     type: 'fire',
-     height:2},             
+        {name: 'Charmander',
+        type: 'fire',
+        height:2},             
     
-    {name: 'Squirtle', 
-     type:' water',
-     height:1.8}
-                  
+        {name: 'Squirtle', 
+         type:' water',
+         height:1.8}
                   ];
+
+//A loop for the above array. This code displays the name and height of each pokemon listed along with a special message for the tallest
+
+for(var i = 0; i <pokemonList.length; i++){
+    if (pokemonList[i].height > 2) {
+    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' This one\'s quite large! </p>'  );
+    } else {
+    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>');
+  }
+};
+  
+                  
+
+   
+    
