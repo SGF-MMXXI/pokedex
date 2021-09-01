ff8ed3a595460bfcd7c9b3372a83debc79154881
SGF-MMXXI <88549986+SGF-MMXXI@users.noreply.github.com>
2021-09-01 10:51:39 -0500

    Added IIFE and new functions for code efficiency
    Nested the original array within the IIFE
    added new functions add and getall()
    replaced the for loop with a for Each loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-//Heres an array of objects listing pokemon and some attributes
+//Heres the IIFE with my pokemonList array inside of it along with the add and get all functions.
+
+let pokemonRepository = (function(){
+
 
 let pokemonList = [{
         name: 'Bulbasaur',
@@ -13,18 +16,29 @@ let pokemonList = [{
          type:' water',
          height:1.8}
                   ];
+    
+     function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+console.log(pokemonRepository.getAll() );
 
 //A loop for the above array. This code displays the name and height of each pokemon listed along with a special message for the tallest
 
-for(var i = 0; i <pokemonList.length; i++){
-    if (pokemonList[i].height > 2) {
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' This one\'s quite large! </p>'  );
-    } else {
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>');
-  }
-};
-  
-                  
 
-   
-    
+pokemonRepository.getAll().forEach(function (pokemon){
+         if (pokemon.height > 2) {
+    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')' + ' This one\'s quite large! </p>'  );
+    } else {
+    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')</p>');            
+                    } });
